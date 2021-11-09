1416b918dd9e041a935a40134884e98e7d818e21
Fabrizzio <fabrizziorp7@gmail.com>
2021-11-09 17:17:51 -0300

    Cambios sobre configuracion terminada

---
+++ models/band.js
@@ -0,0 +1,16 @@
+const { v4 : uuidV4 } = require('uuid');
+
+class Band {
+
+    constructor( name = 'no-name' ){
+
+        this.id = uuidV4(); // identificador unico
+        this.name = name;
+        this.votes = 0;
+
+    }
+
+}
+
+
+module.exports = Band;
